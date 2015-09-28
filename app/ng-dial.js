8e94bfcb5ecc02ed8679bffcc8ab33b69c4ffc11
Tim Rourke <tim@timrourke.com>
2015-09-27 20:09:00 -0500

    Updating math

--- app/ng-dial.js
+++ app/ng-dial.js
@@ -1,43 +1,71 @@
 angular.module('dial', []).
-directive('dial',['$document', '$window', function($document, $window) {
-  return function(scope, element, attr) {
-    var startAngle = 0,
-        w = 200,
-        h = 200,
-        x = (parseInt($window.innerWidth) / 2) - (w / 2), 
-        y = (parseInt($window.innerHeight) / 2) - (h / 2);
-    element.css({
-     position: 'absolute',
-     top: (parseInt($window.innerHeight) / 2) - (w / 2) + 'px',
-     left: (parseInt($window.innerWidth) / 2) - (h / 2) + 'px',
-     transform: 'rotateZ(' + startAngle + 'deg)',
-     borderRadius: '50%',
-     cursor: 'pointer',
-     display: 'block',
-     width: w + 'px',
-     height: h + 'px'
-    });
-    element.on('mousedown', function(event) {
-      // Prevent default dragging of selected content
-      event.preventDefault();
-      $document.on('mousemove', mousemove);
-      $document.on('mouseup', mouseup);
-    });
+controller('dialController', ['$scope', function($scope){
 
-    function mousemove(event) {
-      y = event.clientY;
-      x = event.clientX;
-      ctrX = $window.innerWidth / 2;
-      ctrY = $window.innerHeight / 2;
-      angle = (Math.atan2(ctrY - y, ctrX - x) * 180 / Math.PI) - startAngle;
+  $scope.dialAngle = 0;
+
+  //$scope.$watch(function())
+
+}]).
+directive('dial',['$document', '$window', '$timeout', function($document, $window, $timeout) {
+  return {
+    restrict: 'E',
+    scope: {
+      angle: '='
+    },
+    link: function(scope, element, attrs) {
+      var startAngle = scope.angle,
+          w = 200,
+          h = 200,
+          x = (parseInt($window.innerWidth) / 2) - (w / 2), 
+          y = (parseInt($window.innerHeight) / 2) - (h / 2);
+
       element.css({
-        transform: 'rotateZ(' + (angle + 180) + 'deg)'
+        position: 'absolute',
+        top: (parseInt($window.innerHeight) / 2) - (w / 2) + 'px',
+        left: (parseInt($window.innerWidth) / 2) - (h / 2) + 'px',
+        transform: 'rotateZ(' + startAngle + 'deg)',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        display: 'block',
+        width: w + 'px',
+        height: h + 'px'
       });
-    }
 
-    function mouseup() {
-      $document.off('mousemove', mousemove);
-      $document.off('mouseup', mouseup);
+      scope.$watch('angle', function(newValue, oldValue){
+        updateAngle(newValue - startAngle + 180);
+      });
+
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.clientY;
+        x = event.clientX;
+        ctrX = $window.innerWidth / 2;
+        ctrY = $window.innerHeight / 2;
+        angle = Math.atan2(-(ctrY - y), -(ctrX - x)) * 180 / Math.PI + 180;
+
+        scope.$apply(function(){
+          scope.angle = angle + startAngle - 180;
+        });
+        
+        updateAngle(angle);
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+
+      function updateAngle(angle) {        
+        element.css({
+          transform: 'rotateZ(' + (angle - 180 + startAngle) + 'deg)'
+        });
+      }
     }
-  };
+  }
 }]);
