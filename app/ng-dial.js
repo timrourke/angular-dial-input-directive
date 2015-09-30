4eff7a98d106be664183ee28371381574f5b4507
Tim Rourke <tim@timrourke.com>
2015-09-30 12:45:09 -0500

    Adding improvements to resizability

--- app/ng-dial.js
+++ app/ng-dial.js
@@ -1,15 +1,20 @@
-angular.module('dial', []).
+angular.module('expandingDial', []).
 controller('dialController', ['$scope', function($scope){
 
   $scope.dialAngle = 0;
+  $scope.dialWidth = 200;
+  $scope.dialHeight = 200;
 
 }]).
 directive('dial',['$document', '$window', '$timeout', function($document, $window, $timeout) {
   return {
     restrict: 'E',
     scope: {
-      angle: '='
+      angle: '=',
+      ellipsewidth: '=',
+      ellipseheight: '='
     },
+    template: '<span class="dialSizeHandle"></span><span  class="dialHeightHandle"></span>',
     link: function(scope, element, attrs) {
       var startAngle = scope.angle,
           w = 200,
@@ -32,30 +37,64 @@ directive('dial',['$document', '$window', '$timeout', function($document, $windo
       scope.$watch('angle', function(newValue, oldValue){
         updateAngle(newValue - startAngle);
       });
+      
+      scope.$watch('ellipsewidth', function(newValue, oldValue){
+        updateWidth(newValue);
+      });
+      
+      scope.$watch('ellipseheight', function(newValue, oldValue){
+        updateHeight(newValue);
+      });
 
       element.on('mousedown', function(event) {
         // Prevent default dragging of selected content
         event.preventDefault();
-        $document.on('mousemove', mousemove);
+        
+        if (event.target.className == 'dialSizeHandle') {
+          $document.on('mousemove', resizeEllipse);
+        } else if (event.target.className == 'dialHeightHandle') {
+          $document.on('mousemove', changeEllipseHeight);
+        }
         $document.on('mouseup', mouseup);
       });
+      
+      function changeEllipseHeight(event) {
+        y = event.clientY;
+        x = element[0].offsetLeft + (element[0].clientWidth / 2);
+        ctrX = x;
+        ctrY = element[0].offsetTop + (element[0].clientHeight / 2);
+        ellipseheight = calcLinearDistance({x: x, y: y}, {x: ctrX, y: ctrY}) * 2;
+        
+        updateHeight(ellipseheight);
+        
+        scope.$apply(function(){
+          scope.ellipseheight = ellipseheight;
+        });
+      }
 
-      function mousemove(event) {
+      function resizeEllipse(event) {
         y = event.clientY;
         x = event.clientX;
-        ctrX = $window.innerWidth / 2;
-        ctrY = $window.innerHeight / 2;
+        ctrX = element[0].offsetLeft + (element[0].clientWidth / 2);
+        ctrY = element[0].offsetTop + (element[0].clientHeight / 2);
         angle = Math.atan2(-(ctrY - y), -(ctrX - x)) * 180 / Math.PI + 180;
+        width = calcLinearDistance({x: x, y: y}, {x: ctrX, y: ctrY}) * 2;
 
+        updateAngle(angle);
+        updateWidth(width);
+        
         scope.$apply(function(){
           scope.angle = angle + startAngle;
         });
         
-        updateAngle(angle);
+        scope.$apply(function(){
+          scope.ellipsewidth = width;
+        });
       }
 
       function mouseup() {
-        $document.off('mousemove', mousemove);
+        $document.off('mousemove', resizeEllipse);
+        $document.off('mousemove', changeEllipseHeight);
         $document.off('mouseup', mouseup);
       }
 
@@ -63,6 +102,28 @@ directive('dial',['$document', '$window', '$timeout', function($document, $windo
         element.css({
           transform: 'rotateZ(' + (angle + startAngle) + 'deg)'
         });
+      }
+      
+      function updateWidth(width) {
+        element.css({
+          top: (parseInt($window.innerHeight) / 2) - ((element[0].clientHeight) / 2) + 'px',
+          left: (parseInt($window.innerWidth) / 2) - (width / 2) + 'px',
+          width: width + 'px'
+        });
+      }
+      
+      function updateHeight(height) {
+        element.css({
+          top: (parseInt($window.innerHeight) / 2) - (height / 2) + 'px',
+          left: (parseInt($window.innerWidth) / 2) - ((element[0].clientWidth) / 2) + 'px',
+          height: height + 'px'
+        });
+      }
+      
+      function calcLinearDistance(pointA, pointB) {
+        var xSet = pointB.x - pointA.x;
+        var ySet = pointB.y - pointA.y;
+        return Math.sqrt(Math.pow(xSet, 2) + Math.pow(ySet, 2));
       }
     }
   }
